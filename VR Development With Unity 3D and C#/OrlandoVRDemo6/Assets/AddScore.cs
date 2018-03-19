e9e3b41ed3ab11e282af50664dc00b57f83c216c
Mark S <stick100@hotmail.com>
2018-03-19 10:27:28 -0400

    Add VR Development With Unity 3D and C# with slides and completed project

---
+++ VR Development With Unity 3D and C#/OrlandoVRDemo6/Assets/AddScore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddScore : MonoBehaviour {
+
+    private Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+        scoreText = GameObject.FindWithTag("Score").GetComponent<Text>();
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (scoreText == null)
+            return;
+        var score = int.Parse(scoreText.text);
+        score++;
+        scoreText.text = score.ToString();
+        GameObject.Destroy(this.gameObject);
+    }
+
+}
---
+++ VR Development With Unity 3D and C#/OrlandoVRDemo6/Assets/MoveTowardsGameObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsGameObject : MonoBehaviour {
+
+    public Transform target;
+    
+    // Use this for initialization
+	void Start () {
+        transform.LookAt(target);
+        var rigidBody2 = GetComponent<Rigidbody>();
+        rigidBody2.AddRelativeForce(new Vector3(0, 0, -30));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
